c0b73340f22a0bb7f8a1e433ea06f1faf4c0d3e8
eman <eman@icog-makers.com>
2021-03-25 22:59:05 +0300

    working onehot encoder

--- data/classifier.hs
+++ data/classifier.hs
@@ -1,11 +1,12 @@
-import Data.List
-
+import Data.List as L
+import Data.Map as M
 main :: IO ()
 main = do
   let dataset = ["Hello world", "Is anybody here?", "I hate everyone!", "She is not my friend","I will come tmrw" ]
-  --let tmp = f vocabs
-  print $ vocabs
-  print $ createddictionary f vocabs
+  let vocabs = ["Hello", "world", "Is", "anybody", "here", "I", "hate", "everyone", "She", "is", "I", "will", "come", "not", "my", "friend", "Hello", "I", "hate"] 
+  let result = getindices vocabs
+  print  $ result
+  --print $ b
 
 stopwords = ["the","a", "and", "is", "be", "will","Is"]
 nums :: String
@@ -16,9 +17,9 @@ removestopwords :: [String]  -> [String]
 removestopwords [] = []
 removestopwords (x:xs) = 
 	let tmp = words x
-	    y = filter (`notElem` stopwords) tmp 
+	    y = L.filter (`notElem` stopwords) tmp 
 	    wrap = unwords y 
-	    h = filter (`notElem`  nums) wrap 
+	    h = L.filter (`notElem`  nums) wrap 
 	in  h :removestopwords xs
 --This accepts a list of e.g ["Hello", "world"]
 -- with context 2
@@ -29,7 +30,6 @@ makewordpairs (x:y:z:xs) = (x,y):(x,z):(y,x):(y,z):(z,x):(z,y):makewordpairs xs
 makewordpairs (x:y:xs) =  (x,y):makewordpairs xs
 
 
-vocabs = ["Hello", "world", "Is", "anybody", "here", "I", "hate", "everyone", "She", "is", "I", "will", "come", "not", "my", "friend", "Hello", "I", "hate"]
 --This function removes repeatitive words in a ["the", "Hello"..]
 f :: [String] -> [String]
 f [] = []
@@ -37,14 +37,31 @@ f (x:xs)
 	| x `elem` xs = f xs
 	| otherwise = x:f xs
 
---unique = f vocabs
---sorted = sort unique
---dict = zip  [1..length unique]  sorted
-createddictionary::([String] -> [String]) -> [String] -> [(Int, String)]
-createddictionary f [] = []
-createddictionary f vocabs = do
-			let tmp = f vocabs
-			let sorted = sort tmp
-			zip [1..length tmp] sorted
+createddictionary::([String] -> [String]) -> [String] -> [(String, Int)]
+createddictionary f vocabs = zip  (sort . f $ vocabs) [1..]
 
+vocabs = ["Hello", "world", "Is", "anybody", "here", "I", "hate", "everyone", "She", "is", "I", "will", "come", "not", "my", "friend", "Hello", "I", "hate"] 
+wordpairs = makewordpairs $ vocabs
+xss = L.map  (\(a,b) -> a) wordpairs
+yss = L.map  (\(a,b) -> b) wordpairs
+
+
+dict = M.fromList (createddictionary  f  vocabs )
+getindices:: [String] -> [Int]
+getindices vocabs = L.foldr (\x acc   -> (index x) : acc ) [] vocabs
+index :: String -> Int
+index x=(\(Just i) -> i) $ M.lookup x  dict
+results = getindices vocabs
+zeros:: [Int]
+zeros = L.replicate (length $ getindices vocabs) 0
+
+onehotencoder :: [Int] -> [Int] ->[ [Int]]
+onehotencoder xs zs = L.foldr (\x acc -> insertAt zs 1 x: acc) [] xs
+
+insertAt :: [a] -> a -> Int -> [a]
+insertAt [] elem pos = [elem]
+insertAt (x:xs) elem pos
+    | pos == 1  = elem : x : xs
+    | pos > 1 = x : insertAt xs elem (pos - 1) 
+    | otherwise = x : insertAt xs elem ((pos) + length (x:xs) )
 
